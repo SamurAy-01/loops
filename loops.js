d98c30f28e186882f45e8869755fee8bbff2e508
Ali Samur <97285821+SamurAy-01@users.noreply.github.com>
2022-10-18 15:26:12 +0300

    loops.js
    for döngüsü

---
+++ loops.js
@@ -0,0 +1,59 @@
+//let toplam = 0;
+
+//for(let i = 0; i <= 10; i++){
+//    toplam += i
+//};
+//console.log(toplam);
+
+// let sayilar = [1,4,5,8,4,3,12,5,3,5];
+
+// console.log(sayilar[0] + sayilar[1] + sayilar[2] + sayilar[3] + sayilar[4] + sayilar[5]);
+
+// let toplam = 0;
+
+//for(let i = 0; i < sayilar.length; i++){
+//    toplam += sayilar[i];
+//}
+
+//for(let i in sayilar){
+//    toplam += sayilar[i]
+//console.log(i);
+//}
+
+// for(let sayi of sayilar){
+//     toplam += sayi;
+// }
+
+// console.log(toplam);
+
+// let user = {
+//     "name": "Ali Samur",
+//     "username": "samurai",
+//     "password": "753951",
+//     "email": "alisamur01@gmail.com"
+// };
+
+// for(let key in user){
+//     console.log(key);
+//     console.log(user[key]);
+// }
+/*
+let toplam = 0;
+
+for(let i = 1; i <= 10; i++) {
+    toplam += i;
+    
+}
+*/
+// console.log(sayilar[0] + sayilar[1] + sayilar[2] + sayilar[3] + sayilar[4]);
+/*
+let sayilar = [1,4,5,8,4,3,12,5,3,5];
+let toplam = 0;
+
+for (let i = 0; in sayilar) {
+    toplam += sayilar[i];
+} 
+
+console.log(toplam);
+
+*/
